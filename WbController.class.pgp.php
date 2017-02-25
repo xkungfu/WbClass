c04ac9c7b083a9fc20fd9df5189efc3d6e86598f
Itzcy <itzcy@itzcy.com>
2017-02-25 22:57:01 +0800

    chage WbController FIle

---
+++ WbController.class.pgp.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 微博获取用户的信息
+ *
+ * Created by Itzcy.com.
+ * Project WbClass.
+ * User: Itzcy
+ * Date: 2017/2/25
+ * Time: 22:56
+ */
+
+namespace Phone\Controller;
+
+
+class WbController extends Controller
+{
+
+
+}
